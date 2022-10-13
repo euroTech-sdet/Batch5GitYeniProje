f9db2e98893dd4e8d7b0d62a41c06982eed331cb
Talha Ozleblebici <talha43gs@gmail.com>
2022-10-13 20:41:24 +0100

    Update SeleniumTest.java

--- src/AutomationTool/SeleniumTest.java
+++ src/AutomationTool/SeleniumTest.java
@@ -4,5 +4,15 @@ public class SeleniumTest {
     public static void main(String[] args) {
         System.out.println("TEST1");
         System.out.println("TEST2");
+        // Emre test file 
+        // Emre test file2 
+        // Emre test file3 
+        
+        int test;
+        
+        test=3;
+        
+               
+      
     }
 }
