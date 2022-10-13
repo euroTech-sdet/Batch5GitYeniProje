c52d884c6984d5b5a0337669de556307e28dcd03
Talha <talha43gs@gmail.com>
2022-10-13 20:27:52 +0100

    Talha Selenium methods Uploaded

--- src/AutomationTool/SeleniumTest.java
+++ src/AutomationTool/SeleniumTest.java
@@ -4,5 +4,8 @@ public class SeleniumTest {
     public static void main(String[] args) {
         System.out.println("TEST1");
         System.out.println("TEST2");
+        // talha test method 1
+        //talha test method 2
+        //talha test method3
     }
 }
