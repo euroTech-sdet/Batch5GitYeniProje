043a318cb2e5ca45b72a7d23d2c35229da33f368
Hakan <hakan.sahinler@yahoo.com>
2022-10-14 15:31:16 +0300

    Hakan Test uploaded

---
+++ src/DataFile/Grup3.java
@@ -0,0 +1,9 @@
+package DataFile;
+
+public class Grup3 {
+
+    public static void main(String[] args) {
+        System.out.print("Du hast mich gefragt, du hast mich gefragt");
+    }
+
+}
---
+++ src/DataFile/HakanGrup3_2.java
@@ -0,0 +1,8 @@
+package DataFile;
+
+public class HakanGrup3_2 {
+    public static void main(String[] args) {
+        //Munih Almanya'nın başkentidir.
+        //Test1 tamamlanmadı
+    }
+}
