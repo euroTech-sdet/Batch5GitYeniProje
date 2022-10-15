033d74f34d778ce31279bbe1e7a9239e0587de77
İsaac <isaacsguler.pc@outlook.com>
2022-10-15 20:52:46 +0300

    Different two java files created and completed mine tasks. Created group4 java files. Copleted mine tasks in group tasks...

---
+++ src/Group4_Levent_Omer_Osman_Ishak/Ishak1.java
@@ -0,0 +1,9 @@
+package Group4_Levent_Omer_Osman_Ishak;
+
+public class Ishak1 {
+    public static void main(String[] args) {
+        // Some Test modifications
+        System.out.println("Modications completed by IShak");
+    }
+
+}
