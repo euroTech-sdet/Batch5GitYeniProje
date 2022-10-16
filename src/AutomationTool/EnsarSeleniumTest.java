d4804e2bb9a94282fc8da288ac051e7453837b93
Ensar <ensar2008@hotmail.com>
2022-10-16 10:21:34 +0200

    Genel dosyalar update yapildi

---
+++ src/AutomationTool/EnsarSeleniumTest.java
@@ -0,0 +1,9 @@
+package AutomationTool;
+
+public class EnsarSeleniumTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ensar Selenium dosyasi olusturuldu");
+    }
+}
