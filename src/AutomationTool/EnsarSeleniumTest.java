e9d571b47b08bf2c8cf1b0150db6523551450205
Ensar <ensar2008@hotmail.com>
2022-10-16 22:31:53 +0200

    Ensar dosyalar update yapildi

--- src/AutomationTool/EnsarSeleniumTest.java
+++ src/AutomationTool/EnsarSeleniumTest.java
@@ -5,5 +5,6 @@ public class EnsarSeleniumTest {
     public static void main(String[] args) {
 
         System.out.println("Ensar Selenium dosyasi olusturuldu ");
+
     }
 }
--- src/Pages/AnaSayfa.java
+++ src/Pages/AnaSayfa.java
@@ -1,4 +1,7 @@
 package Pages;
 
 public class AnaSayfa {
+    // ben yarin geliyorum
+    // yasasin dunya
+    // Hersey guzel olacak
 }
