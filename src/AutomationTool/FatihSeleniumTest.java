c0e79453784c37c8d55292da20fa1be77c725833
Fatih3495 <achileus.hektor@icloud.com>
2022-10-16 14:14:30 +0200

    Fatih branchi güncellendi

--- src/AutomationTool/FatihSeleniumTest.java
+++ src/AutomationTool/FatihSeleniumTest.java
@@ -5,5 +5,6 @@ public class FatihSeleniumTest {
     public static void main(String[] args) {
 
         System.out.println("Fatih Selenium File olusturuldu");
+        System.out.println("Test execute edildi");
     }
 }
--- src/DataFile/FatihDataFile.java
+++ src/DataFile/FatihDataFile.java
@@ -5,5 +5,6 @@ public class FatihDataFile {
     public static void main(String[] args) {
 
         System.out.println("Fatih Data file olusturuldu");
+        System.out.println("File update edildi");
     }
 }
