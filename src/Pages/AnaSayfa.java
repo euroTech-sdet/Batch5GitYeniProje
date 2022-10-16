a688bb2f6efd8fc43ee8f8d7454204453be9da30
Fatih3495 <achileus.hektor@icloud.com>
2022-10-16 22:51:25 +0200

    Pages güncellendi

--- src/Pages/AnaSayfa.java
+++ src/Pages/AnaSayfa.java
@@ -1,4 +1,13 @@
 package Pages;
-
 public class AnaSayfa {
-}
+    public static void main(String[] args) {
+
+    }
+   //Ana sayfada ilk kod yazildi
+    // Muhtemelen kodlar cakisacak.
+    // Kodlar cakisirsa bunlari cözmenin yolu var.
+    // Yarin cözeriz
+
+    }
+
+
