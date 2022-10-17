33ef62937ebc5f2b9628961260b2a8e0626e801e
osc428 <115489842+osc428@users.noreply.github.com>
2022-10-17 04:17:46 +0200

    Update SpeciesGroup4.java
    A testing method is added

--- src/Group4_Levent_Omer_Osman_Ishak/SpeciesGroup4.java
+++ src/Group4_Levent_Omer_Osman_Ishak/SpeciesGroup4.java
@@ -4,6 +4,8 @@ public class SpeciesGroup4 {
     public static void main(String[] args) {
        // First Part is mine please continue next tasks
         // Wrote Some codes to Testing Algorithm
+        int conflicts = 4;
+        if ( conflicts > 3 ) System.out.println("I need more coffee");
         // Completed first control
         // Made Modifications
         System.out.println("All article completed by Ishak");
