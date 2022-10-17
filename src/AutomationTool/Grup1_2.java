6750a5d6294fc0ba7ce45c7cb0cac2da8129453b
CanCici <cancici@hotmail.com>
2022-10-17 17:15:08 +0200

    grup1 ıs upload

---
+++ src/AutomationTool/Grup1_2.java
@@ -0,0 +1,11 @@
+package AutomationTool;
+
+public class Grup1_2 {
+    public static void main(String[] args) {
+        System.out.println(" can test" );
+        //for each
+        // for loop
+        // OOP
+
+    }
+}
