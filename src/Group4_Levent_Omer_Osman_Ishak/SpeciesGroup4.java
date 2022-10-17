4ba900012d07480a49716b084a0968ee8205e4dc
osc428 <115489842+osc428@users.noreply.github.com>
2022-10-17 06:16:58 +0200

    Update SpeciesGroup4.java

--- src/Group4_Levent_Omer_Osman_Ishak/SpeciesGroup4.java
+++ src/Group4_Levent_Omer_Osman_Ishak/SpeciesGroup4.java
@@ -4,8 +4,6 @@ public class SpeciesGroup4 {
     public static void main(String[] args) {
        // First Part is mine please continue next tasks
         // Wrote Some codes to Testing Algorithm
-        int conflicts = 4;
-        if ( conflicts > 3 ) System.out.println("I need more coffee");
         // Completed first control
         // Made Modifications
         System.out.print("All article completed by Ishak");
